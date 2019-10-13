502afdd4b1272618e1d9e386c0506f0cc0574026
Erico de Souza Loewe <ed-lowe@hotmail.com>
2019-10-13 09:36:06 -0300

    refactor(server): move to file

---
+++ server/Server.PlayerConnection.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    partial class Server
+    {
+        class PlayerConnection
+        {
+            private Socket socket;
+            private Player player;
+
+            public PlayerConnection(string playerName, Arena arena, Socket socket)
+            {
+                player = new Player(playerName, arena);
+                this.socket = socket;
+                StartCycle().ContinueWith(t => Console.WriteLine("Player died"));
+            }
+
+            private async Task StartCycle()
+            {
+                var task = new Task(() =>
+                {
+                    string command = "";
+
+                    while (!command.ToLower().StartsWith("exit"))
+                    {
+                        var bytes = new byte[1024];
+                        var bytesRec = socket.Receive(bytes);
+
+                        command = Encoding.UTF8.GetString(bytes, 0, bytesRec);
+
+                        try
+                        {
+                            ProcessCommand(command);
+                            string screen = $"{GameCommands.SCREEN}: {player.GetScreenAsString()}";
+                            socket.Send(Encoding.UTF8.GetBytes(screen));
+                        }
+                        catch (Exception ex)
+                        {
+                            socket.Send(Encoding.UTF8.GetBytes("invalid-command\n"));
+                            Console.Write("Stack: ");
+                            Console.WriteLine(ex);
+                        }
+                    }
+
+                    socket.Send(Encoding.UTF8.GetBytes("goodbye\n"));
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                });
+
+                task.Start();
+
+                await task;
+            }
+
+            private void ProcessCommand(string command)
+            {
+                var parsedCommand = command.ToUpper();
+
+                if (parsedCommand.StartsWith(GameCommands.MOVE.ToString()))
+                {
+                    string direction = command.Split(":")[1];
+
+                    if (direction == null)
+                    {
+                        throw new ArgumentException($"Invalid command: {command}");
+                    }
+
+                    MovementDirection parsedDirection = (MovementDirection)Enum.Parse(typeof(MovementDirection), direction.Trim(), true);
+
+                    player.Move(parsedDirection);
+                }
+                else if (parsedCommand.StartsWith(GameCommands.EXIT.ToString()))
+                {
+                    player.Exit();
+                }
+                else
+                {
+                    throw new ArgumentException($"Invalid command: {command}");
+                }
+            }
+        }
+    }
+}
+
--- server/Server.cs
+++ server/Server.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace server
 {
-    class Server
+    partial class Server
     {
         private Arena arena;
         private const int MAX_PLAYERS_IN_THE_GAME = 4;
@@ -74,83 +74,6 @@ namespace server
             listener.Listen(1000);
 
             Console.WriteLine("Socket connected to {0}", localEndPoint.ToString());
-        }
-
-        class PlayerConnection
-        {
-            private Socket socket;
-            private Player player;
-
-            public PlayerConnection(string playerName, Arena arena, Socket socket)
-            {
-                player = new Player(playerName, arena);
-                this.socket = socket;
-                StartCycle().ContinueWith(t => Console.WriteLine("Player died"));
-            }
-
-            private async Task StartCycle()
-            {
-                var task = new Task(() =>
-                {
-                    string command = "";
-
-                    while (!command.ToLower().StartsWith("exit"))
-                    {
-                        var bytes = new byte[1024];
-                        var bytesRec = socket.Receive(bytes);
-
-                        command = Encoding.UTF8.GetString(bytes, 0, bytesRec);
-
-                        try
-                        {
-                            ProcessCommand(command);
-                            string screen = $"{GameCommands.SCREEN}: {player.GetScreenAsString()}";
-                            socket.Send(Encoding.UTF8.GetBytes(screen));
-                        }
-                        catch (Exception ex)
-                        {
-                            socket.Send(Encoding.UTF8.GetBytes("invalid-command\n"));
-                            Console.Write("Stack: ");
-                            Console.WriteLine(ex);
-                        }
-                    }
-
-                    socket.Send(Encoding.UTF8.GetBytes("goodbye\n"));
-                    socket.Shutdown(SocketShutdown.Both);
-                    socket.Close();
-                });
-
-                task.Start();
-
-                await task;
-            }
-
-            private void ProcessCommand(string command)
-            {
-                var parsedCommand = command.ToUpper();
-
-                if (parsedCommand.StartsWith(GameCommands.MOVE.ToString()))
-                {
-                    string direction = command.Split(":")[1];
-
-                    if (direction == null)
-                    {
-                        throw new ArgumentException($"Invalid command: {command}");
-                    }
-
-                    MovementDirection parsedDirection = (MovementDirection)Enum.Parse(typeof(MovementDirection), direction.Trim(), true);
-
-                    player.Move(parsedDirection);
-                }
-                else if (parsedCommand.StartsWith(GameCommands.EXIT.ToString()))
-                {
-                    player.Exit();
-                }
-                else
-                {
-                    throw new ArgumentException($"Invalid command: {command}");
-                }
-            }
         }
     }
 
